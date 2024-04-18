5626b28be11e761681252a862b5c0d72de251a2d
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-04-18 19:05:49 +0100

    feat: added girigiribashi

---
+++ packages/hardhat/contracts/ownable/ShoyuBashi.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.20;
+
+import { ShuSo } from "./ShuSo.sol";
+import { IAdapter } from "../interfaces/IAdapter.sol";
+import { IHashi } from "../interfaces/IHashi.sol";
+import { IShoyuBashi } from "../interfaces/IShoyuBashi.sol";
+
+contract ShoyuBashi is IShoyuBashi, ShuSo {
+    constructor(address _owner, address _hashi) ShuSo(_owner, _hashi) {} // solhint-disable no-empty-blocks
+
+    function setHashi(IHashi _hashi) public override {
+        _setHashi(_hashi);
+    }
+
+    /// @inheritdoc IShoyuBashi
+    function setThreshold(uint256 domain, uint256 threshold) public {
+        _setThreshold(domain, threshold);
+    }
+
+    /// @inheritdoc IShoyuBashi
+    function enableAdapters(uint256 domain, IAdapter[] memory adapters) public {
+        _enableAdapters(domain, adapters);
+    }
+
+    /// @inheritdoc IShoyuBashi
+    function disableAdapters(uint256 domain, IAdapter[] memory adapters) public {
+        _disableAdapters(domain, adapters);
+    }
+
+    /// @inheritdoc IShoyuBashi
+    function getUnanimousHash(uint256 domain, uint256 id) public view returns (bytes32) {
+        return _getUnanimousHash(domain, id);
+    }
+
+    /// @inheritdoc IShoyuBashi
+    function getThresholdHash(uint256 domain, uint256 id) public view returns (bytes32) {
+        return _getThresholdHash(domain, id);
+    }
+
+    /// @inheritdoc IShoyuBashi
+    function getHash(uint256 domain, uint256 id, IAdapter[] memory adapters) public view returns (bytes32) {
+        return _getHash(domain, id, adapters);
+    }
+}
